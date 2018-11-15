c769b3ea36d4d78bccd999fc1ed7bd9ccc731fd5
Nicolas Surian <nicolas.surian@gmail.com>
2018-11-15 21:05:17 +0100

    exo103 - Propriété de servlet

--- week1/exo103/WEB-INF/classes/webcart/WebCartServlet.java
+++ week1/exo103/WEB-INF/classes/webcart/WebCartServlet.java
@@ -22,20 +22,20 @@ public class WebCartServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
         // print a html form using printwriter.
-        String content = "<h1>" + TITLE_PAGE + "</h1>" +
-            "<form action=\"/exo103/cart\" method=\"POST\">" +
+        String content = "<form action=\"/exo103/cart\" method=\"POST\">" +
             "<div>REF: <input type=\"text\" name=\"ref\"></div>" +
             "<div>QTY: <input type=\"text\" name=\"qty\"></div>" +
             "<input type=\"submit\" value=\"Valider\">" +
             "</form>";
 
-        this.writeResponse(res, TITLE_PAGE + " Form", content);
+        this.buildAndWriteResponse(res, TITLE_PAGE + " Form", content, this.myCart);
     }
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
-    throws ServletException, IOException {
+    throws ServletException, IOException, NumberFormatException {
+        String pageTitle = TITLE_PAGE + " POST";
 
         // Get parameters, check null
         String ref = req.getParameter("ref");
@@ -45,31 +45,53 @@ public class WebCartServlet extends HttpServlet {
         if (this.isEmpty(ref) || this.isEmpty(qty)) {
             // print error 400
             res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            content = "Les paramètres [ref] et [qty] sont manquants";
+            content = "ERROR 400 : Les paramètres [ref] et [qty] sont manquants";
+            this.buildAndWriteResponse(res, pageTitle, content);
         } else {
-            // print reference and quantity
-            content = "<h1>" + TITLE_PAGE + "</h1>" +
-                "<div>REF: " + ref + "</div>" +
-                "<div>QTY: " + qty + "</div>" ;
+            this.myCart.addToCart(ref, Integer.parseInt(qty));
+            // sendRedirect to /cart with request dispatcher
+            res.sendRedirect(req.getContextPath() + "/cart");
         }
-
-        this.writeResponse(res, TITLE_PAGE + " Form", content);
     }
 
-    private void writeResponse(HttpServletResponse resp, String title, String content)
+    private void buildAndWriteResponse(HttpServletResponse resp, String title, String content)
         throws IOException {
-        String html = "<!doctype html>\n"+
-            "<html>" +
-            "<head>" +
-            "<meta charset=\"utf-8\">" +
-            "<title>" + title + "</title>" +
-            "</head>" +
-            "<body>" +
-            content +
-            "</body>" +
-            "</html>";
-        PrintWriter out = resp.getWriter();
-        out.write(html);
+            PrintWriter out = this.prepareResponse(resp, title);
+            this.appendTitle(out);
+            out.append(content);
+            this.writeResponse(out);
+    }
+
+    private void buildAndWriteResponse(HttpServletResponse resp, String title, String content, Cart myCart)
+        throws IOException {
+            PrintWriter out = this.prepareResponse(resp, title);
+            this.appendTitle(out);
+            if (!myCart.isEmpty()) {
+                out.append("<h3>Récapitulatif du panier</h3>\n");
+                myCart.print(out);
+            }
+            out.append(content);
+            this.writeResponse(out);
+    }
+
+    private void appendTitle(PrintWriter out) {
+        out.append("<h1>" + TITLE_PAGE + "</h1>\n");
+    }
+
+    private PrintWriter prepareResponse(HttpServletResponse resp, String title)
+        throws IOException {
+            PrintWriter out = resp.getWriter();
+            out.append("<!doctype html>\n");
+            out.append("<html><head>\n");
+            out.append("<meta charset=\"utf-8\">\n");
+            out.append("<title>" + title + "</title>\n");
+            out.append("</head><boby>\n");
+            return out;
+    }
+
+    private void writeResponse(PrintWriter out) {
+        out.append("</body></html>");
+        out.flush();
     }
 
     private boolean isEmpty(String str) {
