1cf0bb8d19c2cfe63130ec1a26f673ca5acc51d8
Nicolas Surian <nicolas.surian@gmail.com>
2018-11-28 19:28:10 +0100

    week3 - exo 301
    end of exercice

--- week3/exo301/src/main/java/fr/eservices/drive/model/Article.java
+++ week3/exo301/src/main/java/fr/eservices/drive/model/Article.java
@@ -1,5 +1,88 @@
 package fr.eservices.drive.model;
 
-public class Article {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "ean" }))
+public abstract class Article {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String ean;
+    private double price;
+    private double vat;
+    private String name;
+    private String img;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getVat() {
+        return vat;
+    }
+
+    public void setVat(double vat) {
+        this.vat = vat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategory(List<Category> categories) {
+        this.categories = categories;
+    }
 
 }
--- week3/exo301/src/main/java/fr/eservices/drive/model/Perishable.java
+++ week3/exo301/src/main/java/fr/eservices/drive/model/Perishable.java
@@ -1,5 +1,29 @@
 package fr.eservices.drive.model;
 
-public class Perishable {
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Perishable extends Article {
+
+    private String lot;
+    private Date bestBefore;
+
+    public Date getBestBefore() {
+        return bestBefore;
+    }
+
+    public void setBestBefore(Date bestBefore) {
+        this.bestBefore = bestBefore;
+    }
+
+    public String getLot() {
+        return lot;
+    }
+
+    public void setLot(String lot) {
+        this.lot = lot;
+    }
 
 }
